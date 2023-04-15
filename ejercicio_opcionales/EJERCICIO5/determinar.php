085712c5665757addf113d2de7a44bf11e0f9ec7
Sebastian Delgadillo <delgadillosebastian75@gmail.com>
2023-04-15 12:51:07 -0400

    Revert "ejercicio_opcionales"
    This reverts commit 1f98255495050fd9d1313fca0d07692fb705dca1.

--- ejercicio_opcionales/EJERCICIO5/determinar.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$n=$_POST['n'];
-
-if($n % 2 == 0){
-    echo "El numero es par";
-}else{
-    echo "El numero es impar";
-}
--- ejercicio_opcionales/EJERCICIO6/mostrar.php
+++
@@ -1,6 +0,0 @@
-<?php
-$nombre=$_GET['nombre'];
-$ciudad=$_GET['ciudad'];
-
-echo '<b> --> '.$nombre. '</b> <br>';
-echo '<FONT COLOR="red"> --> '.$ciudad. '</FONT> <br>';
